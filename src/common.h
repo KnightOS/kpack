fc7fb317ed987daf625f40ce1f6e485babfc314e
matrefeytontias <mattias@refeyton.fr>
2014-09-22 23:19:51 +0200

    Over and over again

--- src/common.h
+++ src/common.h
@@ -15,52 +15,52 @@ typedef struct {
 	int included;
 	versionData version;
 	uint8_t nameLength;
-	char* name;
+	char *name;
 } packageDependency;
 
 typedef struct {
 	uint8_t key;
 	uint8_t vlen;
-	void* value;
+	void *value;
 } metadata;
 
 typedef struct {
 	uint8_t pathLength;
-	char* path;
+	char *path;
 	// Only 24 bits are used
 	uint32_t uncompressedLength;
 	uint32_t compressedLength;
-	FILE* file;
-	char* sumString;
+	FILE *file;
+	char *sumString;
 	uint16_t crc16;
 } filedata;
 
 struct {
 	// Runtime
-	FILE* config;
-	char* configName;
+	FILE *config;
+	char *configName;
 	int pack;
-	char* rootName;
+	char *rootName;
 	//
 	// Package
 	//
-	char* name;
+	char *name;
 	uint8_t version;
 	// Metadata
 	uint8_t mdlen;
-	metadata** md;
+	metadata* *md;
 	// Files
 	uint8_t compressionType;
 	uint8_t sumType;
 	uint8_t flen;
-	filedata** files;
+	filedata* *files;
 } packager;
 
 struct {
 	uint8_t mdlen;
-	metadata** md;
+	metadata* *md;
 	// This doesn't include the files' contents
-	filedata** files;
+	filedata* *files;
 } packageStub;
 
 enum {

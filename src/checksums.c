219d26baee50a3d4088fb28848e6187c622ca5b7
matrefeytontias <mattias@refeyton.fr>
2014-09-29 01:23:54 +0200

    Fixed checksum (again)

--- src/checksums.c
+++ src/checksums.c
@@ -1,18 +1,20 @@
 #include "common.h"
+#include <string.h>
 
 uint16_t calculateCRC16(FILE *in) {
 	uint16_t crcVal = 0xffff;
 	int b, c, backupSize = ftell(in);
-	
+    
 	fseek(in, 0, SEEK_SET);
-	
-	while((c = fgetc(in)) != EOF) {
-		for(b = 0; b < 8; b++, c <<= 1) {
-			if((crcVal ^ (c >> 7)) & 0x8000) {
+    
+	while ((c = fgetc(in)) != EOF) {
+		for(b = 0; b < 8; b++) {
+			if ((crcVal >> 15) ^ (c >> 7)) {
 				crcVal = (crcVal << 1) ^ 0x68da;
 			} else {
 				crcVal <<= 1;
 			}
+            c <<= 1;
 		}
 	}
 	
